71aeb07eb2c98f1aa2141b751927483ee425ff7c
Simon Fontaine <74004726+Simon-Fontaine@users.noreply.github.com>
2022-10-14 12:38:04 +0200

    last edits

--- main.js
+++ main.js
@@ -1,36 +1,33 @@
-function test() {
-  const MSG_INPUT = "Veuillez introduire votre mot, appuyez sur RETURN pour terminer !";
+function sortList() {
+  const MSG_INPUT = "Veuillez introduire votre mot, appuyez sur CANCEL pour abandonner !";
   const MSG_ABANDON = "Abandon demandé - Fin du programme !";
   const MSG_ERREUR = "Erreur, veuillez ne pas introduire de chiffres";
 
   let allInputs = [];
   let input;
 
-  while (input != 0) {
+  while (input != "STOP") {
     input = prompt(MSG_INPUT);
 
     if (input === null) {
-      return console.log(MSG_ABANDON);
+      return alert(MSG_ABANDON);
     } else if (input == -1) {
       allInputs.pop();
       console.log(allInputs);
       continue;
     } else {
-      while (!isNaN(String(input)) && input != 0) {
+      while (!isNaN(String(input)) && input != "STOP") {
         input = prompt(MSG_ERREUR);
       }
 
       input = input.replace(/[^a-zA-Z]/g, "");
 
       allInputs.push(input);
-
-      console.log(input);
-      console.log(allInputs);
     }
   }
 
   allInputs.pop();
   allInputs.sort();
 
-  console.log("Votre liste : " + allInputs);
+  alert("Votre liste : " + allInputs.join(", "));
 }
